a00797fc060b58e6929ff0e0f9f97c6dc4caad51
SmartPolarBear <clevercoolbear@outlook.com>
2023-03-24 11:24:01 +0800

    fix problems

--- experiment2/flex/lexer.h
+++ experiment2/flex/lexer.h
@@ -1,5 +1,23 @@
 #pragma once
 
-int install_id() ;
-int install_num() ;
+#ifdef __cplusplus
+#include <cstdio>
+#else
+#include <stdio.h>
+#endif
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+int install_id();
+int install_num();
+
+extern FILE *yyin;
+
+extern int yylex(void);
+
+#ifdef __cplusplus
+}
+#endif
--- experiment2/flex/main.cc
+++ experiment2/flex/main.cc
@@ -1,6 +1,15 @@
 #include "lexer.h"
 
-int main()
+int main(int argc, char **argv)
 {
+	yyin = fopen(argv[1], "r");
+	fseek(yyin, 0, SEEK_SET);
+
+	int token = 0;
+	while ((token = yylex()) != 0);
+
+	fclose(yyin);
+	yyin = nullptr;
+
 	return 0;
 }
